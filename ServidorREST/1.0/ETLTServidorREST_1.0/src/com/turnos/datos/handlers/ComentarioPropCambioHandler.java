52cfc0ebc102cd7b33716d5156fd6d21349408d7
catoyos-turnos <catoyos+turnos@gmail.com>
2016-05-12 01:36:09 +0200

    propCAmbioHandler, Comentario

--- ServidorREST/1.0/ETLTServidorREST_1.0/src/com/turnos/datos/handlers/ComentarioPropCambioHandler.java
+++ ServidorREST/1.0/ETLTServidorREST_1.0/src/com/turnos/datos/handlers/ComentarioPropCambioHandler.java
@@ -1,5 +1,19 @@
 package com.turnos.datos.handlers;
 
+//92xxxx
 public class ComentarioPropCambioHandler extends GenericHandler{
-
+	private static final String QUERY_LISTA_COMENTARIOS_PROP =
+		"SELECT com.id_comentario_pc, com.id_prop_cambio, "
+		+ "com.id_usuario, GET_NOMBRE_USUARIO(com.id_usuario) "
+		+ "FROM comentario_prop_cambio com "
+		+ "WHERE com.id_prop_cambio=? "
+		+ "ORDER BY com.hora ";
+	
+	private static final String UPDATE_INSERT_COMENTARIO = 
+		"INSERT INTO comentario_prop_cambio "
+		+ "(id_prop_cambio, id_usuario, hora, texto) "
+		+ "VALUES ?,?,?,?";
+	private static final String UPDATE_DELETE_COMENTARIO = 
+		"DELETE FROM comentario_prop_cambio com"
+		+ "WHERE com.id_comentario_pc=?";
 }
--- ServidorREST/1.0/ETLTServidorREST_1.0/src/com/turnos/datos/handlers/PropCambioHandler.java
+++ ServidorREST/1.0/ETLTServidorREST_1.0/src/com/turnos/datos/handlers/PropCambioHandler.java
@@ -1,5 +1,23 @@
 package com.turnos.datos.handlers;
 
+//91xxxx
 public class PropCambioHandler extends GenericHandler{
+	private static final String QUERY_LISTA_PROP_CAMBIOS_USUARIO =
+		"SELECT com.id_comentario_pc, com.id_prop_cambio, "
+		+ "com.id_usuario, GET_NOMBRE_USUARIO(com.id_usuario) "
+		+ "FROM comentario_prop_cambio com "
+		+ "WHERE com.id_prop_cambio=? "
+		+ "ORDER BY com.hora ";
 
+	private static final String UPDATE_INSERT_PROP_CAMBIOS = 
+		"INSERT INTO comentario_prop_cambio "
+		+ "(id_prop_cambio, id_usuario, hora, texto) "
+		+ "VALUES ?,?,?,?";
+	private static final String UPDATE_UPDATE_PROP_CAMBIOS = 
+			"UPDATE propuesta_cambio "
+			+ "(id_prop_cambio, id_usuario, hora, texto) "
+			+ "VALUES ?,?,?,?";
+	private static final String UPDATE_DELETE_PROP_CAMBIOS = 
+		"DELETE FROM comentario_prop_cambio com"
+		+ "WHERE com.id_comentario_pc=?";
 }
