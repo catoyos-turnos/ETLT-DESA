8e346ad260f011972d6cd935708e81602546e2aa
catoyos-turnos <catoyos+turnos@gmail.com>
2016-05-30 18:13:37 -0400

    cliente 000

--- ClienteAPI/1.0/ETLTClienteAPI_1.0/src/com/turnos/cliente/Propiedades.java
+++ ClienteAPI/1.0/ETLTClienteAPI_1.0/src/com/turnos/cliente/Propiedades.java
@@ -4,22 +4,33 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Propiedades {
-	private static final String BUNDLE_NAME_DEFAULT;
-	private static final ResourceBundle RESOURCE_BUNDLE_DEFAULT;
+	//private static final String BUNDLE_NAME_DEFAULT;
+	//private static final ResourceBundle RESOURCE_BUNDLE_DEFAULT;
 	private static final String BUNDLE_NAME_USER;
 	private static final ResourceBundle RESOURCE_BUNDLE_USER;
 
 	static {
 		ResourceBundle aux = null;
-		BUNDLE_NAME_DEFAULT = "resources.config"; //$NON-NLS-1$
-		try {
-			aux = ResourceBundle.getBundle(BUNDLE_NAME_DEFAULT);
-		} catch(Exception e) {
-			aux = null;
-			e.printStackTrace();
-		}
-		RESOURCE_BUNDLE_DEFAULT = aux;
+		//BUNDLE_NAME_DEFAULT = "resources.config"; //$NON-NLS-1$
+		//try {
+		//	aux = ResourceBundle.getBundle(BUNDLE_NAME_DEFAULT);
+		//} catch(Exception e) {
+		//	aux = null;
+		//	e.printStackTrace();
+		//}
+		//RESOURCE_BUNDLE_DEFAULT = aux;
 		
+		/* **** SE DEBE INCLUIR UN FICHERO config.properties EN LA CARPETA 
+				EN LA QUE SE SITUE EL JAR, CON AL MENOS LA DIRECCION BASE
+				Y LAS CLAVES PÚBLICA Y SECRETA DE LA APLICACIÓN. P.EJ:
+				
+				
+				Aplicacion.baseURL=http://localhost:9090/ETLTServidorREST/api
+				Aplicacion.publicKey=8A21F0C7ADF164DD10CD
+				Aplicacion.secretKey=F6E8EE83AD21DF2B09F8
+				
+				
+				**** */
 		BUNDLE_NAME_USER = "config"; //$NON-NLS-1$
 		try {
 			aux = ResourceBundle.getBundle(BUNDLE_NAME_USER);
--- ClienteAPI/1.0/ETLTClienteAPI_1.0/src/com/turnos/cliente/modelo/Turno.java
+++ ClienteAPI/1.0/ETLTClienteAPI_1.0/src/com/turnos/cliente/modelo/Turno.java
@@ -7,7 +7,20 @@ import com.turnos.datos.vo.TurnoBean;
 
 public class Turno implements Serializable {
 	private static final long serialVersionUID = 47L;
-
+	
+	private TurnoBean beanOriginal;
+	private TurnoBean beanAux;
+	private boolean flagNueva;
+	private boolean flagModificada;
+	private boolean flagBorrada;
+	
+	private Turno(TurnoBean residencia, boolean nueva) {
+		this.beanOriginal = residencia;
+		this.flagNueva = nueva;
+		this.flagModificada = false;
+		this.flagBorrada = false;
+		this.beanAux = new TurnoBean();
+	}
 
 	public static List<Turno> listaTurnos (String codRes) {
 		//TODO
@@ -38,5 +51,84 @@ public class Turno implements Serializable {
 		//TODO
 		return null;
 	}
+
+	public static List<Servicio> addServicio (String codRes, ServicioBean servRaw, String codTurno) {
+		//TODO
+		return null;
+	}
+
+	public static List<Servicio> deleteServicio (String codRes, String codTurno) {
+		//TODO
+		return null;
+	}
 	
+	
+	public String getCodTurno() {
+		if(flagBorrada)
+			return null;
+		else if(flagNueva)
+			return beanOriginal.getCodTurno();
+		else if(flagModificada && beanAux.getCodTurno()!=null)
+			return beanAux.getCodTurno();
+		else return beanOriginal.getCodTurno();
+	}
+
+	public String getCodResidencia() {
+		if(flagBorrada)
+			return null;
+		else if(flagNueva)
+			return beanOriginal.getCodResidencia();
+		else if(flagModificada && beanAux.getCodResidencia()!=null)
+			return beanAux.getCodResidencia();
+		else return beanOriginal.getCodResidencia();
+	}
+
+	public TipoTurno getTipo() {
+		if(flagBorrada)
+			return null;
+		else if(flagNueva)
+			return TipoTurno.safeValueOf(beanOriginal.getTipo());
+		else if(flagModificada && beanAux.getTipo()!=null)
+			return TipoTurno.safeValueOf(beanAux.getTipo());
+		else return TipoTurno.safeValueOf(beanOriginal.getTipo());
+	}
+
+	public void setCodTurno(String codTurno) {
+		this.codTurno = codTurno;
+	}
+
+	public void setCodResidencia(String codResidencia) {
+		this.codResidencia = codResidencia;
+	}
+
+	public void setTipo(TipoTurno tipo) {
+		this.tipo = TipoTurno.safeValueOf(tipo);
+	}
+
+	public List<Servicio> getServicios (String codRes, String codTurno) {
+		//TODO
+		return null;
+	}
+
+	public boolean addServicio (String codRes, ServicioBean servRaw, String codTurno) {
+		//TODO
+		return null;
+	}
+
+	public boolean deleteServicio (String codRes, String codTurno) {
+		//TODO
+		return null;
+	}
+
+
+	public static Turno genera(TurnoBean bean) {
+		if (bean == null) {
+			return null;
+		} else return new Turno(bean, false);
+	}
+
+	public static Turno nuevo() {
+		return new Turno(new TurnoBean(), true);
+	}
+
 }
